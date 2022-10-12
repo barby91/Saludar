a2f1064d4b5b7359455e8956301fe0791efa85b3
barby_91 <barby_91_89@hotmail.es>
2022-10-12 14:15:13 +0200

    saludo personalizado

--- app/src/main/java/dia/uned/saludar/ActividadPrincipal.java
+++ app/src/main/java/dia/uned/saludar/ActividadPrincipal.java
@@ -1,8 +1,13 @@
 package dia.uned.saludar;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import org.w3c.dom.Text;
 
 public class ActividadPrincipal extends AppCompatActivity {
     @Override
@@ -12,5 +17,14 @@ public class ActividadPrincipal extends AppCompatActivity {
 
         //establecemos que se use la vista adecuada
         setContentView(R.layout.vista_actividad_principal);
+    }
+
+    public void metodfoSaludar (View view)
+    {
+        EditText entrada = (EditText) findViewById(R.id.entradaNombre);
+        TextView salida = (TextView) findViewById(R.id.textoSalida);
+        String nombre = entrada.getText().toString();
+        String inicioSaludo = getString(R.string.inicio_saludo);
+        salida.setText(inicioSaludo + " " + nombre + "!");
     }
 }
